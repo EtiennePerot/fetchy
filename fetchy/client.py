fbfce299f7fc0af80e97a4c9350792ea7e14c5f3
AlassaneNdiaye <none@none>
2011-12-13 22:14:44 -0500

    Created client.py

--- fetchy/client.py
+++ fetchy/client.py
@@ -1,8 +1,21 @@
 import urllib2
-class Client:
-    _isReady = False
-    def asyncFetch(url):
-        html = urllib2.urlopen(url)
+import threading
+
+class client:
+    def __init__(self, url):
+        self._url = url
+    def run(self):
+        html = urllib2.urlopen(self._url)
         print html.read()
-    def isReady():
-        return _isReady
+
+class _runClient:
+    def __init__(self):
+        pass
+    def run(self, url):
+        _client = client(url)
+        thread = threading.Thread(target=_client.run(), args=())
+        thread.start()
+
+
+
+
